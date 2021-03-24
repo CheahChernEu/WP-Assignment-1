295a56b6e34d04572a6e8a0291bf933f78cbbd0e
CHEAH CHERN EU <tennischeah@gmail.com>
2021-03-24 09:42:27 +0800

    Title change to signIn in login.php

--- logIn.php
+++ logIn.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login Homepage</title>
+	<title>SignIn Homepage</title>
 	<link rel="stylesheet" type="text/css" href="loginStyling.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
